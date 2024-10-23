fb6e50b37b6b8fdfadbb376ddd550307b34f5ab7
Max Saparov <max.saparov@gmail.com>
2024-10-23 18:44:20 -0400

    new franz porotocol

--- c/franzclient.c
+++ c/franzclient.c
@@ -1,93 +1,117 @@
 #include "franzclient.h"
-#include <stddef.h>
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <netinet/in.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <arpa/inet.h>
-#include <sys/types.h>
-#include <fcntl.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
+#include <unistd.h>
 
-FranzReceiver new_FranzReceiver(unsigned short port, char* sa_data) {
-	int socketfd;
-	FranzReceiver rx;
-	struct sockaddr_in addr;
+franz_consumer_t franz_consumer_new(char *addr, uint16_t port, char *topic,
+                                    int16_t group) {
+  int32_t socketfd;
+  uint32_t len;
+  char *handshake;
+  franz_consumer_t rx;
+  struct sockaddr_in sa_addr;
 
-	addr.sin_family = AF_INET;
-	addr.sin_port = htons(port);
-	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-	addr.sin_zero[8] = '\0';
+  sa_addr.sin_family = AF_INET;
+  sa_addr.sin_port = htons(port);
+  sa_addr.sin_addr.s_addr = inet_addr(addr);
+  // sa_addr.sin_zero[8] = '\0';
 
-	socketfd = socket(AF_INET, SOCK_STREAM, 0);
+  socketfd = socket(AF_INET, SOCK_STREAM, 0);
 
-	if ((connect(socketfd, (struct sockaddr*)&addr, sizeof(struct sockaddr))) == -1) {
-		fprintf(stderr, "fuck\n");
-	};
+  if ((connect(socketfd, (struct sockaddr *)&sa_addr,
+               sizeof(struct sockaddr))) == -1) {
+    perror("couldn't connect to broker");
+  };
 
-	rx.socket = fdopen(socketfd, "r+");
-	char handshake[] = "1\ntest\n";
-	fwrite(handshake, sizeof(char), strlen(handshake), rx.socket);
+  rx.socket = fdopen(socketfd, "r+");
 
-	return rx;
+  handshake = calloc(1024, sizeof(char));
+  if (group == -1) {
+    char fmt[] = "version=1,topic=%s,api=consume";
+    snprintf(handshake, 1024, fmt, topic);
+  } else {
+    char fmt[] = "version=1,topic=%s,api=consume,group=%lu";
+    snprintf(handshake, 1024, fmt, topic, group);
+  }
+
+  len = htonl(strlen(handshake));
+  fwrite(&len, sizeof(uint32_t), 1, rx.socket);
+  fwrite(handshake, sizeof(char), strlen(handshake), rx.socket);
+  fflush(rx.socket);
+
+  return rx;
 }
 
-FranzSender new_FranzSender(unsigned short port, char* sa_data) {
-	int socketfd;
-	FranzSender tx;
-	struct sockaddr_in addr;
+franz_producer_t franz_producer_new(char *addr, uint16_t port, char *topic) {
+  int32_t socketfd;
+  uint32_t len;
+  franz_producer_t tx;
+  struct sockaddr_in sa_addr;
 
-	addr.sin_family = AF_INET;
-	addr.sin_port = htons(port);
-	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-	addr.sin_zero[8] = '\0';
+  sa_addr.sin_family = AF_INET;
+  sa_addr.sin_port = htons(port);
+  sa_addr.sin_addr.s_addr = inet_addr(addr);
+  // sa_addr.sin_zero[8] = '\0';
 
-	socketfd = socket(AF_INET, SOCK_STREAM, 0);
+  socketfd = socket(AF_INET, SOCK_STREAM, 0);
 
-	if ((connect(socketfd, (struct sockaddr*)&addr, sizeof(struct sockaddr))) == -1) {
-		fprintf(stderr, "fuck\n");
-	};
+  if ((connect(socketfd, (struct sockaddr *)&sa_addr,
+               sizeof(struct sockaddr))) == -1) {
+    perror("couldn't connect to broker");
+    exit(1);
+  };
 
-	tx.socket = fdopen(socketfd, "r+");
-	char handshake[] = "0\ntest\n";
-	fwrite(handshake, sizeof(char), strlen(handshake), tx.socket);
+  tx.socket = fdopen(socketfd, "r+");
 
-	return tx;
+  char fmt[] = "version=1,topic=%s,api=produce";
+  char *handshake = calloc(1024, sizeof(char));
+  snprintf(handshake, 1024, fmt, topic);
+  len = htonl(strlen(handshake));
+
+  fwrite(&len, sizeof(uint32_t), 1, tx.socket);
+  fwrite(handshake, sizeof(char), strlen(handshake), tx.socket);
+  fflush(tx.socket);
+
+  return tx;
 }
 
-void push(FranzSender tx, char* data, size_t len) {
-	char* buf;
-	buf = malloc(len + 2);
-	memcpy(buf, data, len + 1);
-	strcat(buf, "\n");
-
-	fprintf(stderr, "%d\n", fwrite(buf, sizeof(char), strlen(buf), tx.socket));
+void franz_send(franz_producer_t tx, char *data, size_t len) {
+  fwrite(data, sizeof(char), len, tx.socket);
+  fwrite("\n", sizeof(char), strlen("\n"), tx.socket);
 }
 
-char* pop(FranzReceiver rx) {
-	char* lineptr;
-	size_t n;
+void franz_send_unbuffered(franz_producer_t tx, char *data, size_t len) {
+  franz_send(tx, data, len);
+  fflush(tx.socket);
+}
 
-	lineptr = NULL;
-	n = 0;
-	getline(&lineptr, &n, rx.socket);
-	return lineptr;
+char *franz_recv(franz_consumer_t rx) {
+  char *msg;
+  size_t n;
+
+  msg = NULL;
+  n = 0;
+  getline(&msg, &n, rx.socket);
+  return msg;
 }
 
 int main() {
-	FranzSender tx;
-	FranzReceiver rx;
-	tx = new_FranzSender(8085, "127.0.0.1");
+  franz_producer_t tx;
+  franz_consumer_t rx;
+  tx = franz_producer_new("127.0.0.1", 8085, "test");
 
-	push(tx, "asdf", 4);
-	push(tx, "mostlypacket", 12);
+  franz_send_unbuffered(tx, "asdf", 4);
+  franz_send_unbuffered(tx, "mostlypacket", 12);
 
-	rx = new_FranzReceiver(8085, "127.0.0.1");
+  rx = franz_consumer_new("127.0.0.1", 8085, "test", -1);
 
-	while(1) {
-		fprintf(stderr, "%s", pop(rx));
-	}
+  while (1) {
+    fprintf(stderr, "%s", franz_recv(rx));
+  }
 }
-
-
--- c/franzclient.h
+++ c/franzclient.h
@@ -1,19 +1,21 @@
 // TODO: IFDEF
 //
+#include <stdint.h>
 #include <stdio.h>
 
-typedef struct FranzReceiver {
-	FILE* socket;
-} FranzReceiver;
+typedef struct FranzConsumer {
+  FILE *socket;
+} franz_consumer_t;
 
+typedef struct FranzProducer {
+  FILE *socket;
+} franz_producer_t;
 
-typedef struct FranzSender {
-	FILE* socket;
-} FranzSender;
+franz_consumer_t franz_consumer_new(char *addr, uint16_t port, char *topic,
+                                    int16_t group);
 
-FranzReceiver new_FranzReceiver(unsigned short port, char* sa_data);
-char* pop(FranzReceiver rx);
+char *franz_recv(franz_consumer_t rx);
 
-FranzSender new_FranzSender(unsigned short port, char* sa_data);
-void push(FranzSender tx, char* data, size_t len);
-
+franz_producer_t franz_producer_new(char *addr, uint16_t port, char *topic);
+void franz_send(franz_producer_t tx, char *data, size_t len);
+void franz_send_unbuffered(franz_producer_t tx, char *data, size_t len);
